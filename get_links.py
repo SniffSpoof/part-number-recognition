29c057181a523cca340de3e7979848efac8279c8
SniffSpoof <52812804+SniffSpoof@users.noreply.github.com>
2024-12-19 21:06:45 +0700

    Add files via upload

---
+++ get_links.py
@@ -0,0 +1,15 @@
+from picker_model import TargetModel
+from collect_data import collect_links
+import json
+
+def get_links(car_brand="toyota", max_pages=3, max_links=15, offset=0):
+    with open('/content/part-number-recognition/prompts.json', 'r') as f:
+      prompts = json.load(f)
+
+    #car_brand = 'toyota'
+    first_page_link = prompts[car_brand.lower()]['first_page_url']
+
+    t = TargetModel()
+    links = collect_links(t, first_page_link, max_pages=max_pages, max_links=max_links, offset=offset)
+    #print(links)
+    return links
